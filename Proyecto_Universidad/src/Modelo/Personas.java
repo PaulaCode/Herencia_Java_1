0c712720c5fe2d6bbd8d1f3a011b098072d6ac9a
PaulaCode <paulacode01@gmail.com>
2020-07-31 12:31:21 -0500

    Agregando atributos a persona

--- Proyecto_Universidad/src/Modelo/Personas.java
+++ Proyecto_Universidad/src/Modelo/Personas.java
@@ -10,5 +10,24 @@ package Modelo;
  * @author paula
  */
 public class Personas {
+    private int identificacion_persona;
+    private String nombre_persona;
+
+    public int getIdentificacion_persona() {
+        return identificacion_persona;
+    }
+
+    public void setIdentificacion_persona(int identificacion_persona) {
+        this.identificacion_persona = identificacion_persona;
+    }
+
+    public String getNombre_persona() {
+        return nombre_persona;
+    }
+
+    public void setNombre_persona(String nombre_persona) {
+        this.nombre_persona = nombre_persona;
+    }
+    
     
 }
