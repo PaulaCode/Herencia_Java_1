80512452c570465d541a118f70618a4de752a218
PaulaCode <paulacode01@gmail.com>
2020-07-31 14:09:16 -0500

    cambios jessica

--- Proyecto_Universidad/src/Modelo/Profesores.java
+++ Proyecto_Universidad/src/Modelo/Profesores.java
@@ -1,10 +1,22 @@
 
 package Modelo;
 
-/**
- *
- * @author paula
- */
-public class Profesores {
+import Vista.InOut;
+
+public class Profesores extends Empleados {
+    
+    private int numero_tarjeta;
+    private InOut ioData = new InOut();
+    public Profesores(int identificacion_profesor,String nombre_profesor,int numero_tarjeta)
+    {
+        super(identificacion_profesor,nombre_profesor);
+        this.numero_tarjeta=numero_tarjeta;
+    }
+    
+    public void dictarClase()
+    {
+        ioData.mostrarResultado(super.getNombre_persona()+" dicta clase");
+    }
+    
     
 }
