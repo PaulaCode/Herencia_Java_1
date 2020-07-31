a201ecdef0cd52f11f221d5a7fd3f423c96b01c2
PaulaCode <paulacode01@gmail.com>
2020-07-31 13:21:47 -0500

    Agregando extends

--- Proyecto_Universidad/src/Modelo/Empelados.java
+++ Proyecto_Universidad/src/Modelo/Empelados.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
-/**
- *
- * @author paula
- */
-public class Empelados {
+
+public class Empelados extends Personas{
+    
     
 }
